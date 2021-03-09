a30d729d37138bb63741c6802bdfebc6c22c8d69
okamotoshun <okamons8811@gmail.com>
2021-03-09 23:18:30 +0900

    homepage完成

---
+++ src/page/HomePage.jsx
@@ -0,0 +1,37 @@
+import { useHistory } from 'react-router';
+
+import {
+  Button,
+  Container,
+  Header,
+  Icon,
+  Image,
+  Segment,
+} from 'semantic-ui-react';
+
+import './HomePage.css';
+
+const HomePage = () => {
+  const history = useHistory();
+  return (
+    <div className="homepage">
+      <Segment inverted textAlign="center" vertical >
+        <Container>
+          <Header as="h1" inverted>
+            <Image
+              src="/assets/birthday.png"
+              style={{ marginBottom: 15 }}
+            />
+            Birth days
+          </Header>
+          <Button onClick={() => history.push('/login')} size="huge" inverted>
+            Get Started
+            <Icon name="right arrow" inverted />
+          </Button>
+        </Container>
+      </Segment>
+    </div>
+  );
+};
+
+export default HomePage;
